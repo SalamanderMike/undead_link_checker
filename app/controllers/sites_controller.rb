39679bd90a09af53a06ae07b3f9469b5bf3589b7
salamandermike <salamandermike@gmail.com>
2014-09-09 22:42:04 -0700

    up to nokogiri

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,16 +1,25 @@
 class SitesController < ApplicationController
+  require 'open-uri'
+
+
   def new
+    @site = Site.new
     render :new
   end
 
   def create
-
-
-
-
+    url_params = params.require(:site).permit(:url)
+    site = Site.create(url_params)
+    redirect_to site_path(site.id)
   end
 
   def show
+    id = params[:id]
+    @site = Site.find(id)
+    @link = Array.new
+    url = open(@site.url).read
+
+    @page = Nokogiri::HTML(url)
 
 
 
